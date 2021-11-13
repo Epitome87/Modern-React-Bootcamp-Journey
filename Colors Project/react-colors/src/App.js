8003c897334402a5892859bbec3870765a578e3d
Matthew McGrath <rubywep@hotmail.com>
2021-11-13 12:05:26 -0800

    Add React Router for home route.  Next is to make it work with palette routes based on the palette's ID.

--- Colors Project/react-colors/src/App.js
+++ Colors Project/react-colors/src/App.js
@@ -1,14 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Palette from './Palette';
 import seedPalettes from './seedPalettes';
 import { generatePalette } from './colorHelpers';
 
 function App() {
-  console.log(generatePalette(seedPalettes[3]));
   return (
-    <div className='App'>
-      <Palette palette={generatePalette(seedPalettes[4])} />
-    </div>
+    <Routes>
+      <Route path='/' element={<h1>Palette List Goes Here</h1>} />
+      <Route
+        path='/palette/:id'
+        element={<Palette palette={generatePalette(seedPalettes[4])} />}
+      />
+    </Routes>
   );
 }
 
