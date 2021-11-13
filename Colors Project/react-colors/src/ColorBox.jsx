851e009c0775e9d6929d98f445d687b1193f55de
Matthew McGrath <rubywep@hotmail.com>
2021-11-12 17:02:42 -0800

    Add the styling / animation for the ColorBox's copy-overlay functionality. This occurs when the user clicks to copy the color to the clipboard, at which point that same color grows as a div with some text in it.

--- Colors Project/react-colors/src/ColorBox.jsx
+++ Colors Project/react-colors/src/ColorBox.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard'; // Easy Clipboard functionality
 
 const ColorBox = (props) => {
+  const [isCopied, setIsCopied] = useState(false);
   const { name, background } = props;
+
+  const changeCopyState = () => {
+    setIsCopied(true);
+
+    // Set copy back to false after a few moments
+    // TODO: useEffect for this?
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  };
 
   return (
     // Wrap in CopyToClipBoard, which will copy the value of background when Component is clicked
-    <CopyToClipboard text={background}>
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
       <div className='ColorBox' style={{ background }}>
+        <div
+          className={`copy-overlay ${isCopied && 'show'}`}
+          style={{ background }}
+        />
+        <div className={`copy-msg ${isCopied && 'show'}`}>
+          <h1>Copied!</h1>
+          <p>{background}</p>
+        </div>
         <div className='copy-container'>
           <div className='box-content'>
             <span>{name}</span>
