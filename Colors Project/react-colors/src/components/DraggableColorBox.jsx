5a126115065717997479c6dc4995c82a64fe84aa
Matthew McGrath <rubywep@hotmail.com>
2021-11-14 21:12:12 -0800

    Style DraggableColorBox. Give it a MUI icon for future delete functionality.

--- Colors Project/react-colors/src/components/DraggableColorBox.jsx
+++ Colors Project/react-colors/src/components/DraggableColorBox.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { withStyles } from '@mui/styles';
 import draggableColorBoxStyles from '../styles/DraggableColorBoxStyles';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 function DraggableColorBox({ color, name, classes }) {
   return (
     <div className={classes.DraggableColorBox}>
-      {color} - {name}
+      <div className={classes.boxContent}>
+        <span>
+          {name} - {color}
+        </span>
+        <span className={classes.deleteIcon}>
+          <DeleteForeverIcon />
+        </span>
+      </div>
     </div>
   );
 }
--- Colors Project/react-colors/src/styles/DraggableColorBoxStyles.jsx
+++ Colors Project/react-colors/src/styles/DraggableColorBoxStyles.jsx
@@ -8,7 +8,41 @@ const styles = {
     position: 'relative',
     cursor: 'pointer',
     marginBottom: '-3.5px',
+
+    // Seem to need to do this, rather than relying on the deleteIcon class
+    '& svg': {
+      color: 'rgba(0, 0, 0, 0.5)',
+      transition: 'all 0.3s ease-in-out',
+    },
+
+    '&:hover svg': {
+      color: 'white',
+      transform: 'scale(1.5)',
+    },
   },
+
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    padding: '10px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+
+    // Unique to DraggableColorBox over ColorBox
+    display: 'flex',
+    alignItems: "center",
+    justifyContent: 'space-between',
+  },
+
+  // Applies properly to the DeleteForeverIcon, but doesn't allow our "&:hover svg" class to transition properly
+  // deleteIcon: {
+  // color: 'rgba(0, 0, 0, 0.5)',
+  // transition: 'all 1s ease-in-out',
+  // },
 };
 
 export default styles;
