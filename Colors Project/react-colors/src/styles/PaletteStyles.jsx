68279729fb582b89556ceb53987b93f6beb53f99
Matthew McGrath <rubywep@hotmail.com>
2021-11-15 17:53:35 -0800

    Make ColorBox a little more responsive.

--- Colors Project/react-colors/src/styles/PaletteStyles.jsx
+++ Colors Project/react-colors/src/styles/PaletteStyles.jsx
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 const styles = {
   Palette: {
     height: '100vh',
@@ -39,6 +41,21 @@ const styles = {
       border: 'none',
       textDecoration: 'none',
       transition: 'opacity 0.5s linear',
+    },
+
+    [sizes.down('lg')]: {
+      width: '75%',
+      height: '33.3333%',
+    },
+
+    [sizes.down('md')]: {
+      width: '50%',
+      height: '20%',
+    },
+
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: '10%',
     },
   },
 };
