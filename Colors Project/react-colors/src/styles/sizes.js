136da227aea9d1dd05160495af2843c196feb810
Matthew McGrath <rubywep@hotmail.com>
2021-11-15 17:41:18 -0800

    Add responsive helper for media queries. Some responsive styling of the ColorBox.

---
+++ Colors Project/react-colors/src/styles/sizes.js
@@ -0,0 +1,23 @@
+export default {
+  up(size) {
+    const sizes = {
+      sx: '575.98px',
+      sm: '767.98px',
+      md: '991.98px',
+      lg: '119.98px',
+    };
+
+    return `@media (min-width: ${sizes[size]})`;
+  },
+
+  down(size) {
+    const sizes = {
+      xs: '575.98px',
+      sm: '767.98px',
+      md: '991.98px',
+      lg: '1119.98px',
+    };
+
+    return `@media (max-width: ${sizes[size]})`;
+  },
+};
